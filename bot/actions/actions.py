f10631d9ae448bcb9d8bd0361559b1e8e6c3ab80
amandeep-singh-12 <amansaini19064@gmail.com>
2021-05-26 12:55:40 +0530

    Forms added

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -25,3 +25,34 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, EventType
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ValidateResponse(Action):
+    def name(self) -> Text:
+        return "user_details_form"
+    
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+     ) -> List[EventType]:
+        required_slots = ["name"]
+
+        for slot_name in required_slots:
+            if tracker.slot.get(slot_name) is None:
+                return [SlotSet("requested_slot", slot_name)]
+        
+        return [SlotSet("requested_slot", None)]
+
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template= "utter_details_thanks", Name = tracker.get_slot("name"))
