87caf5e57954b34cb62f74c7003bdc523fdf4102
amandeep-singh-12 <amansaini19064@gmail.com>
2021-06-14 20:26:49 +0530

    Actions for sending email

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -33,7 +33,9 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet, EventType
 from rasa_sdk.executor import CollectingDispatcher
 import webbrowser
-
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class ActionVideo(Action):
@@ -48,25 +50,76 @@ class ActionVideo(Action):
         return []
 
 
-class ValidateResponse(Action):
-    def name(self) -> Text:
-        return "user_details_form"
-    
-    def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
-     ) -> List[EventType]:
-        required_slots = ["name", "email"]
 
-        for slot_name in required_slots:
-            if tracker.slot.get(slot_name) is None:
-                return [SlotSet("requested_slot", slot_name)]
-        
-        return [SlotSet("requested_slot", None)]
 
+#class ValidateResponse(Action):
+#    def name(self) -> Text:
+#        return "user_details_form"
+#    
+#    def run(
+#        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+#     ) -> List[EventType]:
+#        required_slots = ["name", "email"]
+#
+#        for slot_name in required_slots:
+#            if tracker.slot.get(slot_name) is None:
+#                return [SlotSet("requested_slot", slot_name)]
+#        
+#        return [SlotSet("requested_slot", None)]
+#
+#
+#class ActionSubmit(Action):
+#    def name(self) -> Text:
+#        return "action_submit"
+#
+#    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+#        dispatcher.utter_message(template= "utter_details_thanks", Name = tracker.get_slot("name"), Email = tracker.get_slot("email"))
+#
 
 class ActionSubmit(Action):
     def name(self) -> Text:
         return "action_submit"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template= "utter_details_thanks", Name = tracker.get_slot("name"), Email = tracker.get_slot("email"))
+    def run(
+        self,
+        dispatcher,
+        tracker : Tracker,
+        domain : "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+
+        SendEmail(
+            tracker.get_slot("email"),
+            tracker.get_slot("subject"),
+            tracker.get_slot("message")
+        )
+        dispatcher.utter_message("Thank you for providing details. I have sent your mail at {}".format(tracker.get_slot("email")))
+
+
+def SendEmail(toaddr, subject, message):
+    fromaddr = "amandeep1219210@jmit.ac.in"
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+
+    msg['To'] = toaddr
+
+    msg['Subject'] = subject
+
+    body = message
+
+    msg.attach(MIMEText(body, 'plain'))
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    try:
+        s.login(fromaddr, "College123@")
+
+        # Converts the Multipart msg into a string
+        text = msg.as_string()
+
+        # sending the mail
+        s.sendmail(fromaddr, toaddr, text)
+    except:
+        print("An Error occured while sending email.")
+    finally:
+        # terminating the session
+        s.quit()
